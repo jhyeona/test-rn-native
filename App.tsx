8a1a92e6601e0feee8f4bb390a588656dd6d9082
hyeonah <84828218+jhyeona@users.noreply.github.com>
2024-01-09 18:53:08 +0900

    IMHR-67 recoil settings
    recoil settings
    - 리코일 설치
    - 디렉토리 생성
       src/recoil/.../atoms, index
    - type 은 src/types/... 로 사용

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import 'react-native-gesture-handler';
+import {RecoilRoot} from 'recoil';
 import RootStackNavigation from './src/navigation/RootStackNavigation';
 
 function App(): React.JSX.Element {
   return (
-    <>
+    <RecoilRoot>
       <RootStackNavigation />
-    </>
+    </RecoilRoot>
   );
 }
 
--- src/container/schedule/index.tsx
+++ src/container/schedule/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {SafeAreaView, Text, TouchableOpacity} from 'react-native';
 import {BottomTabNavigationHelpers} from '@react-navigation/bottom-tabs/lib/typescript/src/types';
+import {useRecoilValue} from 'recoil';
+import testState from '../../recoil/Schedule';
 
 const Schedule = ({navigation}: {navigation: BottomTabNavigationHelpers}) => {
+  const testData = useRecoilValue(testState.testTestState);
   const test = () => {
     navigation.navigate('SignIn');
   };
   return (
     <SafeAreaView>
       <Text>Schedule</Text>
+      <Text>RecoilTestText: {testData.text}</Text>
       <TouchableOpacity onPress={test}>
         <Text>LOGOUT</Text>
       </TouchableOpacity>
