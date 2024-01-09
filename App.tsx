61802aa9b15ad20c48f20856ef38f5b64b696499
hyeonah <84828218+jhyeona@users.noreply.github.com>
2024-01-09 19:25:34 +0900

    IMHR-67 react-query 설치

--- App.tsx
+++ App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {RecoilRoot} from 'recoil';
 import RootStackNavigation from './src/navigation/RootStackNavigation';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 function App(): React.JSX.Element {
   return (
     <RecoilRoot>
-      <RootStackNavigation />
+      <QueryClientProvider client={queryClient}>
+        <RootStackNavigation />
+      </QueryClientProvider>
     </RecoilRoot>
   );
 }
