60f5a477fc90a9a2a2276e396e48fed7ecf5eb29
hyeonah <84828218+jhyeona@users.noreply.github.com>
2024-01-11 20:28:48 +0900

    IMHR-69 API 연결 / 추가 퍼블리싱
    - 휴대폰번호 중복확인 / TAS / SMS 인증 / SMS 인증 완료 / 회원가입 API 추가
    - 변경된 필드에 맞춰 퍼블리싱 추가+변경

--- src/hooks/useSignUp.ts
+++ src/hooks/useSignUp.ts
@@ -1,6 +1,32 @@
-import {requestPostSignUp} from '../apis/signUp.ts';
+import {
+  requestPostSignUp,
+  requestPostSignUpPhone,
+  requestPostSignUpSMSCode,
+  requestPostSignUpSMSConfirm,
+  requestPostSignUpTAS,
+} from '../apis/SignUp.ts';
 
-export const postSignUpPhone = async (args: {url: string; data: {}}) => {
+export const postSignUpPhone = async (args: {data: {}}) => {
+  const response = await requestPostSignUpPhone(args);
+  return response.data;
+};
+
+export const postSignUpTAS = async (args: {data: {}}) => {
+  const response = await requestPostSignUpTAS(args);
+  return response.data;
+};
+
+export const postSignUpSMS = async (args: {data: {}}) => {
+  const response = await requestPostSignUpSMSCode(args);
+  return response.data;
+};
+
+export const postSignUpSMSConfirm = async (args: {data: {}}) => {
+  const response = await requestPostSignUpSMSConfirm(args);
+  return response.data;
+};
+
+export const postSignUp = async (args: {data: {}}) => {
   const response = await requestPostSignUp(args);
   return response.data;
 };
