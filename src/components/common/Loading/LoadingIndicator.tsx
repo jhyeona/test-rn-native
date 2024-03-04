df0cbb0ea08ded59e28f00b67a7041f774bfa18b
hyeonah <84828218+jhyeona@users.noreply.github.com>
2024-03-04 15:55:43 +0900

    IMHR-96 로딩스피너 변경
    - 코어 컴포넌트의 ActivityIndicator 를 자체 제작으로 변경(Android 구 기종에서 ActivityIndicator 컴포넌트 인식 불가)

--- src/components/common/Loading/LoadingIndicator.tsx
+++ src/components/common/Loading/LoadingIndicator.tsx
@@ -1,26 +1,53 @@
 import React from 'react';
-import {ActivityIndicator, StyleSheet, View} from 'react-native';
+import {View, StyleSheet, Animated, Easing} from 'react-native';
 import {useRecoilValue} from 'recoil';
 import globalState from '#recoil/Global';
 import {COLORS} from '#constants/colors.ts';
 
 const LoadingIndicator = () => {
   const isLoading = useRecoilValue(globalState.globalLoadingState);
+  const spinValue = new Animated.Value(0);
+
+  Animated.loop(
+    Animated.timing(spinValue, {
+      toValue: 1,
+      duration: 1000,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }),
+  ).start();
+
+  const spin = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
   return (
     <>
       {isLoading && (
-        <View
-          style={{
-            ...StyleSheet.absoluteFillObject,
-            justifyContent: 'center',
-            alignItems: 'center',
-            // backgroundColor: 'rgba(0, 0, 0, 0.3)',
-          }}>
-          <ActivityIndicator size="large" color={COLORS.primary} />
+        <View style={styles.container}>
+          <Animated.View
+            style={[styles.spinner, {transform: [{rotate: spin}]}]}
+          />
         </View>
       )}
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  spinner: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 4,
+    borderColor: COLORS.primary,
+    borderTopColor: 'transparent',
+  },
+});
 
 export default LoadingIndicator;
