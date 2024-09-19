b6399758024d4ec846f190aebd520b49ef40f650
zoey <84828218+jhyeona@users.noreply.github.com>
2024-09-19 15:59:22 +0900

    IMHR-176 기관선택 컴포넌트 분리

--- src/containers/DailySchedules/index.tsx
+++ src/containers/DailySchedules/index.tsx
@@ -1,44 +1,23 @@
-import {useEffect, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 
 import {BottomTabNavigationHelpers} from '@react-navigation/bottom-tabs/lib/typescript/src/types';
-import {useRecoilState} from 'recoil';
 
 import WeeklyCalendar from '#components/Calendar/WeeklyCalendar.tsx';
 import CButton from '#components/common/CommonButton/CButton.tsx';
 import CSafeAreaView from '#components/common/CommonView/CSafeAreaView.tsx';
 import CView from '#components/common/CommonView/CView.tsx';
-import Dropdown, {ItemProps} from '#components/common/Dropdown/Dropdown.tsx';
+import AcademySelector from '#components/Schedule/AcademySelector.tsx';
 import ScheduleHeader from '#components/Schedule/ScheduleHeader.tsx';
 import Academy from '#containers/Academy';
 import DaySchedules from '#containers/DailySchedules/components/DaySchedules.tsx';
 import {useGetUserInfo} from '#hooks/useUser.ts';
-import GlobalState from '#recoil/Global';
 
 const DailySchedule = ({
   navigation,
 }: {
   navigation: BottomTabNavigationHelpers;
 }) => {
-  const [academyList, setAcademyList] = useState([{label: '', id: ''}]);
-  const [selectAcademy, setSelectAcademy] = useRecoilState(
-    GlobalState.selectedAcademy,
-  );
   const {userData} = useGetUserInfo();
-
-  useEffect(() => {
-    if (userData && userData.studentList.length > 0) {
-      const newList: Array<ItemProps> = [];
-      userData?.studentList.map(val => {
-        newList.push({
-          label: val.academy.name,
-          id: String(val.academy.academyId),
-        });
-      });
-      setAcademyList(newList); // 기관 선택 리스트
-      setSelectAcademy(newList[0].id); // 기관의 기본 선택 데이터는 첫번째 기관
-    }
-  }, [userData]);
 
   return (
     <>
@@ -47,14 +26,7 @@ const DailySchedule = ({
           <CSafeAreaView>
             <ScheduleHeader />
             <CView>
-              <Dropdown
-                items={academyList}
-                onSelect={item => setSelectAcademy(item.id)}
-                selected={
-                  academyList.filter(val => val.id === String(selectAcademy))[0]
-                }
-                disabled={userData ? userData.studentList.length <= 0 : false}
-              />
+              <AcademySelector />
               <View style={styles.container}>
                 <WeeklyCalendar />
                 <DaySchedules />
--- src/containers/WeeklySchedules/index.tsx
+++ src/containers/WeeklySchedules/index.tsx
@@ -1,52 +1,15 @@
-import React, {useEffect, useState} from 'react';
-
-import {useRecoilState, useRecoilValue} from 'recoil';
-
 import TimeTable from '#components/Calendar/TimeTable.tsx';
 import CSafeAreaView from '#components/common/CommonView/CSafeAreaView.tsx';
 import CView from '#components/common/CommonView/CView.tsx';
-import Dropdown from '#components/common/Dropdown/Dropdown.tsx';
+import AcademySelector from '#components/Schedule/AcademySelector.tsx';
 import ScheduleHeader from '#components/Schedule/ScheduleHeader.tsx';
-import GlobalState from '#recoil/Global';
-import userState from '#recoil/User';
 
 const WeeklySchedules = () => {
-  const userData = useRecoilValue(userState.userInfoState);
-  const [academyList, setAcademyList] = useState([{label: '', id: ''}]);
-  const [selectAcademy, setSelectAcademy] = useRecoilState(
-    GlobalState.selectedAcademy,
-  );
-
-  const onChangeDropList = (item: {label: string; id: string}) => {
-    setSelectAcademy(Number(item.id));
-  };
-
-  useEffect(() => {
-    if (userData && userData.studentList.length > 0) {
-      const newList: Array<{label: string; id: string}> = [];
-      userData?.studentList.map(val => {
-        newList.push({
-          label: val.academy.name,
-          id: String(val.academy.academyId),
-        });
-      });
-      setAcademyList(newList); // 기관 선택 리스트
-      setSelectAcademy(Number(newList[0].id)); // 기관의 기본 선택 데이터는 첫번째 기관
-    }
-  }, [userData]);
-
   return (
     <CSafeAreaView>
       <ScheduleHeader />
       <CView>
-        <Dropdown
-          items={academyList}
-          onSelect={onChangeDropList}
-          selected={
-            academyList.filter(val => val.id === String(selectAcademy))[0]
-          }
-          disabled={userData ? userData.studentList.length <= 0 : false}
-        />
+        <AcademySelector />
         <TimeTable />
       </CView>
     </CSafeAreaView>
