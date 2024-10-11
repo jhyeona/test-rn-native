a0c07607977a6642795719f5dba0b21f6ffcda0e
zoey <84828218+jhyeona@users.noreply.github.com>
2024-10-11 16:22:31 +0900

    IMHR-187 기관 변경 컴포넌트 수정
    - 전반적인 수정 및 업데이트는 변경이 있을 때만 업데이트 하도록 최적화

--- src/components/Schedule/AcademySelector.tsx
+++ src/components/Schedule/AcademySelector.tsx
@@ -1,4 +1,5 @@
 import {useEffect, useState} from 'react';
+import {Alert} from 'react-native';
 
 import {useRecoilState} from 'recoil';
 
@@ -7,12 +8,17 @@ import {useGetUserInfo} from '#hooks/useUser.ts';
 import GlobalState from '#recoil/Global';
 
 const AcademySelector = () => {
-  const [academyList, setAcademyList] = useState([{label: '', id: ''}]);
+  const {userData} = useGetUserInfo();
   const [selectAcademy, setSelectAcademy] = useRecoilState(
     GlobalState.selectedAcademy,
   );
 
-  const {userData} = useGetUserInfo();
+  const [academyList, setAcademyList] = useState([{label: '', id: ''}]);
+
+  const onSelectAcademy = (item: ItemProps) => {
+    Alert.alert(`${item.label} 기관이 선택되었습니다.`);
+    setSelectAcademy(item.id);
+  };
 
   useEffect(() => {
     if (userData && userData.studentList.length > 0) {
@@ -23,15 +29,26 @@ const AcademySelector = () => {
           id: String(val.academy.academyId),
         });
       });
-      setAcademyList(newList); // 기관 선택 리스트
-      setSelectAcademy(newList[0].id); // 기관의 기본 선택 데이터는 첫번째 기관
+
+      // 변경된 경우에만 업데이트
+      const isListChanged =
+        newList.length !== academyList.length ||
+        newList.some((item, index) => item.id !== academyList[index]?.id);
+      if (isListChanged) {
+        setAcademyList(newList);
+      }
+
+      // 기본 선택이 없는 경우 첫 번째 기관 선택
+      if (!selectAcademy) {
+        setSelectAcademy(newList[0].id);
+      }
     }
   }, [userData]);
 
   return (
     <Dropdown
       items={academyList}
-      onSelect={item => setSelectAcademy(item.id)}
+      onSelect={onSelectAcademy}
       selected={academyList.filter(val => val.id === String(selectAcademy))[0]}
       disabled={userData ? userData.studentList.length <= 0 : false}
     />
